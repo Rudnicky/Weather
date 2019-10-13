46ccf8051df1e403266f348e6608bab0be6ac5d5
Pawel Rudnicki <p.j.rudnicki@gmail.com>
2019-10-13 02:26:47 +0200

    Added navigation usage within the child view

--- Weather/Weather/ViewModels/MainViewModel.cs
+++ Weather/Weather/ViewModels/MainViewModel.cs
@@ -1,12 +1,23 @@
-﻿using Weather.Interfaces;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Weather.Interfaces;
 using Weather.ViewModels.Base;
+using Xamarin.Forms;
 
 namespace Weather.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        public ICommand GoToChildViewButtonClickedCommand => new Command(async () => await GoToChildViewButtonClicked());
+
         public MainViewModel(INavigationService navigationService) : base(navigationService)
         {
+        }
+
+        private async Task GoToChildViewButtonClicked()
+        {
+            await base.NavigationService.NavigateToAsync<ChildViewModel>();
         }
     }
 }
