990d591460e8961817c71f1c8105fdb2ad113b6b
Pawel Rudnicki <p.j.rudnicki@gmail.com>
2019-10-13 02:50:33 +0200

    Added EventToCommandBehavior & TextChanged with args

--- Weather/Weather/ViewModels/ChildViewModel.cs
+++ Weather/Weather/ViewModels/ChildViewModel.cs
@@ -1,12 +1,26 @@
-﻿using Weather.Interfaces;
+﻿using System;
+using System.Windows.Input;
+using Weather.Interfaces;
 using Weather.ViewModels.Base;
+using Xamarin.Forms;
 
 namespace Weather.ViewModels
 {
     public class ChildViewModel : ViewModelBase
     {
+        public ICommand TextChangedCommand => new Command((object arg) => TextChanged(arg));
+
         public ChildViewModel(INavigationService navigationService) : base(navigationService)
         {
+        }
+
+        private void TextChanged(object arg)
+        {
+            if (arg is TextChangedEventArgs textChangedArgs)
+            {
+                var newValue = textChangedArgs.NewTextValue;
+                var oldValue = textChangedArgs.OldTextValue;
+            }
         }
     }
 }
